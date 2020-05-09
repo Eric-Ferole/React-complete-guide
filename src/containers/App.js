d4fb058b79094d4a27bce66573121565937e9768
EricFerole <EricFerole@MacBook-Pro-de-Eric-2.local>
2020-05-09 13:08:52 -0400

    Reference

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,7 +21,9 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0,
+    authenticated: false
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -62,7 +64,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => { 
+      return {
+      persons: persons,
+      changeCounter: prevState.changeCounter +1 
+      }
+    });
   };
 
   deletePersonHandler = personIndex => {
@@ -77,6 +84,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({authenticated: true})
+  }
+
   render() {
     console.log('[App.js] render');
     let persons = null;
@@ -87,6 +98,7 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
       );
     }
@@ -105,6 +117,7 @@ class App extends Component {
                   showPersons={this.state.showPersons}
                   personsLength={this.state.persons.length}
                   clicked={this.togglePersonsHandler}
+                  login={this.loginHandler}
                 /> : null }
         {persons}
       </Aux>
